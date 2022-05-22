e0219426c8418bf1d5b8c16269ee39db8ae32ecb
kenkomu <komukenneth216@gmail.com>
2022-05-22 11:59:15 -0700

    printf

--- print_str.c
+++ print_str.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_nbr - print number
  * @ap: arg list
@@ -82,3 +83,182 @@ int print_percent(va_list args __attribute__((unused)))
 {
 	return (_putchar('%'));
 }
+
+/**
+ * print_binary - print number binary base
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_binary(va_list ap)
+{
+	char *str;
+	int sum = 0;
+	unsigned int argument = va_arg(ap, unsigned int);
+
+	if (!argument)
+	{
+		sum += _puts("0", 0);
+		return (sum);
+	}
+	str = convert_base(argument, 2, 0);
+	if (!str)
+		return (0);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
+
+/**
+ * print_octal - print number octal base
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_octal(va_list ap)
+{
+	char *str;
+	int sum;
+
+	str = convert_base(va_arg(ap, unsigned int), 8, 0);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
+/**
+ * print_hexa_lower - print hexa lower
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_hexa_lower(va_list ap)
+{
+	char *str;
+	int sum;
+
+	str = convert_base(va_arg(ap, unsigned int), 16, 0);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
+
+/**
+ * print_hexa_upper - print hexa upper
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_hexa_upper(va_list ap)
+{
+	char *str;
+	int sum;
+
+	str = convert_base(va_arg(ap, unsigned int), 16, 1);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
+/**
+ * print_unsigned - manage unsigned int
+ * @ap: arg list
+ * Return: value number of printed chars
+ */
+
+int print_unsigned(va_list ap)
+{
+	unsigned int argument = va_arg(ap, unsigned int);
+
+	return (print_unsigned_number(argument));
+}
+/**
+ * print_str_unprintable - unprint some characters
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_str_unprintable(va_list ap)
+{
+	char *argument = va_arg(ap, char *);
+	int sum = 0;
+
+	if (!argument)
+	{
+		sum += _puts("(null)", 0);
+		return (sum);
+	}
+
+	return (_puts(argument, 1));
+}
+
+/**
+ * print_str_reverse - reverse a string
+ * @ap: arg list
+ * Return: number printed char
+ */
+int print_str_reverse(va_list ap)
+{
+	char *argument = va_arg(ap, char *), *str;
+	int size, left, limit, right, sum = 0;
+
+	if (!argument)
+	{
+		sum += _puts("%r", 0);
+		return (sum);
+	}
+
+	size = _strlen_recursion(argument);
+	right = size - 1;
+	limit = (size % 2 == 0) ? (size + 1) / 2 : (size / 2);
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	if (size % 2 != 0)
+	{
+		str[limit] = argument[limit];
+	}
+
+	for (left = 0; left < limit; left++)
+	{
+		str[left] = argument[right];
+		str[right] = argument[left];
+		right--;
+	}
+
+	str[size] = '\0';
+
+	sum = _puts(str, 0);
+	free(str);
+
+	return (sum);
+}
+
+/**
+ * print_rot13 - print string with rot13
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_rot13(va_list ap)
+{
+	int sum = 0;
+	char *str, *argument = va_arg(ap, char*);
+
+	if (!argument)
+	{
+		sum += _puts("%R", 0);
+		return (sum);
+	}
+
+	str = convert_rot13(argument);
+	if (!str)
+		return (0);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
+
