1d99fc8847a033b5aa26f12bc4a4d9a106359839
kenkomu <komukenneth216@gmail.com>
2022-05-22 10:18:25 -0700

    print_str.c

--- print_per.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-int print_per(va_list l_strings)
-{
-	return (_putchar('%'));
-}
--- print_str.c
+++ print_str.c
@@ -1,15 +1,84 @@
 #include "main.h"
-#include <stdarg.h>
+/**
+ * print_nbr - print number
+ * @ap: arg list
+ * Return: number of printed char
+ */
 
-int print_str(va_list l_strings)
+int print_nbr(va_list ap)
 {
-	int k = 0;
-	char *s = va_arg(l_strings, char *);
+	return (print_number(va_arg(ap, int)));
+}
+/**
+ * print_ptr - print adress
+ * @ap: arg void
+ * Return: number of printed char
+ */
+int print_ptr(va_list ap)
+{
+	char *str;
+	int sum = 0;
+	unsigned long p = (unsigned long) va_arg(ap, void*);
 
-	while (s[k] != '\0')
+	if (!p)
 	{
-		_putchar(s[k]);
-		k++;
+		sum += _puts("(nil)", 0);
+		return (sum);
 	}
-	return (0);
+
+	str = convert_base_pointer(p);
+	if (!str)
+		return (0);
+	sum += _puts("0x", 0);
+	sum += _puts(str, 0);
+	free(str);
+	return (sum);
+}
+/**
+ * print_char - print single char
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+
+	if (c == '\0')
+	{
+		return (write(1, &c, 1));
+	}
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_str - print string
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_str(va_list ap)
+{
+	char *argument = va_arg(ap, char *);
+	int sum = 0;
+
+	if (!argument)
+	{
+		sum += _puts("(null)", 0);
+		return (sum);
+	}
+
+	return (_puts(argument, 0));
+}
+
+/**
+ * print_percent -  print percent character
+ * @args: char
+ * Return: percent character
+ */
+
+int print_percent(va_list args __attribute__((unused)))
+{
+	return (_putchar('%'));
 }
