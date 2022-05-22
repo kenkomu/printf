5aebf69b6a00666caaacb6cffa96127a57b1525d
kenkomu <komukenneth216@gmail.com>
2022-05-22 06:38:27 -0700

    changes

--- main.h
+++ main.h
@@ -2,8 +2,15 @@
 #define MAIN_H
 
 #include <stdarg.h>
-
+#include <stdio.h>
+#include <string.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _print_char(va_list ap);
+int print_number(va_list*);
+int print_numb(int n);
+int print_string(va_list ap);
+int print_percent(va_list arg __attribute__((unused)));
+int puts (const char *__s);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,49 +1,74 @@
 #include "main.h"
-/*
- * *_printf - function that produces output according to a format
- * *@format: character string
- * *return: number of chacaracters printed
- * */
-int _printf(const char *format, ...)
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * print_string - print string
+ * @ap: arg list
+ * return: number of printed char
+ */
+int print_string(va_list ap)
 {
-	va_list l_strings;
-	int char_count = 0;
-	int i, n;
-	char* ptr;
+	char *arg = va_arg(ap, char *);
+	int sum = 0;
 
-	va_start(l_strings, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	if (!arg)
 	{
-		if (format[i] == '%')
-		{
-			switch (format[i + 1])
-			{
-			case 'c':
-				_putchar(va_arg(l_strings, int));
-				i++;
-				break;
-
-			case 's':
-				ptr = va_arg(l_strings, char *);
-				for (n = 0; ptr[n] != '\0'; n++)
-					_putchar(ptr[n]);
-				i++;
-				break;
-
-			case '%':
-				i++;
-				_putchar('%');
-				break;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			char_count += 1;
-		}
+		sum += _printf("(null)", 0);
+		return (sum);
 	}
 
-	va_end(l_strings);
-	return (char_count);
+return (_printf(arg, 0));
 }
+
+/**
+ * print_percent -print percent
+ * arg: char
+ * return: percent character
+ */
+int print_percent(va_list arg __attribute__((unused)))
+{
+	return (_putchar('%'));
+}
+
+/**
+ * print_number - print number
+ * @ap: arg list
+ * return: number of printed char
+ */
+int print_number(va_list int)
+{
+	return (print_number(va_arg(ap, int)));
+}
+
+/**
+ * print_numb - diplays the number contained in the integer
+ * @n: int to print
+ * return: number of printed char
+ */
+int print_numb(int n)
+{
+	unsigned int nb;
+	int count = 0;
+
+	nb = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		nb = -nb;
+	}
+	if (nb <= 9)
+	{
+		count += _putchar(nb + '0');
+		return (count);
+	}
+	if (nb > 9)
+	{
+		count += print_number(nb / 10) + 1;
+		_putchar(nb % 10 + '0');
+		return (count);
+	}
+	return (0);
+}
+
