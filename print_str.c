0f360c7a33e463688aa0dd4acd5657164899f41f
kenkomu <komukenneth216@gmail.com>
2022-05-22 11:20:53 -0700

    Printf

--- print_str.c
+++ print_str.c
@@ -82,15 +82,3 @@ int print_percent(va_list args __attribute__((unused)))
 {
 	return (_putchar('%'));
 }
-/**
- * print_unsigned - manage unsigned int
- * @ap: arg list
- * Return: value number of printed chars
- */
-
-int print_unsigned(va_list ap)
-{
-	unsigned int argument = va_arg(ap, unsigned int);
-
-	return (print_unsigned_number(argument));
-}
